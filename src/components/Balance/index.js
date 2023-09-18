f1d59025b4c0fb15febd0c04fab064efa253a1c3
Gabriel Souza <104937852+gabrielsouzas@users.noreply.github.com>
2023-09-18 15:16:27 -0300

    Lista de movimentações e botões menu

--- src/components/Balance/index.js
+++ src/components/Balance/index.js
@@ -1,22 +1,22 @@
 import React from "react";
 import { StyleSheet, View, Text } from "react-native";
 
-export default function Balance() {
+export default function Balance({ saldo, gastos }) {
     return (
         <View style={styles.container}>
             <View style={styles.item}>
                 <Text style={styles.itemTitle}>Saldo</Text>
-                <View>
+                <View style={styles.content}>
                     <Text style={styles.currencySymbol}>R$</Text>
-                    <Text style={styles.balance}>15.000,00</Text>
+                    <Text style={styles.balance}>{saldo}</Text>
                 </View>
             </View>
 
             <View style={styles.item}>
                 <Text style={styles.itemTitle}>Gastos</Text>
-                <View>
+                <View style={styles.content}>
                     <Text style={styles.currencySymbol}>R$</Text>
-                    <Text style={styles.balance}>1.000,00</Text>
+                    <Text style={styles.expenses}>{gastos}</Text>
                 </View>
             </View>
         </View>
@@ -32,13 +32,31 @@ const styles = StyleSheet.create({
         paddingEnd: 18,
         marginTop: -24,
         marginStart: 14,
-        marginEnd14,
+        marginEnd: 14,
         borderRadius: 4,
         paddingTop: 22,
         paddingBottom: 22,
+        zIndex: 99,
     },
-    item: {},
+    item: {
+        fontSize: 20,
+        color: "#DADADA",
+    },
     itemTitle: {},
-    currencySymbol: {},
-    balance: {},
+    content: {
+        flexDirection: "row",
+        alignItems: "center",
+    },
+    currencySymbol: {
+        color: "#DADADA",
+        marginRight: 6,
+    },
+    balance: {
+        fontSize: 22,
+        color: "#2ecc71",
+    },
+    expenses: {
+        fontSize: 22,
+        color: "#e74c3c",
+    },
 });
